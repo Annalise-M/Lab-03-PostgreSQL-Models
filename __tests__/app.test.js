f4701a9f9880a570a4dedb44befb6ba3a0a94ea6
Annalise Murphy <nali247@gmail.com>
2020-09-08 17:28:36 -0700

    CRUD POST test and route passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,26 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const Llama = require('../lib/models/llama');
 
 describe('PostgreSQL-Models routes', () => {
   beforeEach(() => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
   });
+
+  it('create a new Llama via POST', async() => {
+    const response = await request(app)
+      .post('/api/v1/llamas')
+      .send({ name: 'sammy', age: 10, weight: '300 lbs' });
+    
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      name: 'sammy',
+      age: 10,
+      weight: '300 lbs'
+    });
+  });
+
+
+
 });
